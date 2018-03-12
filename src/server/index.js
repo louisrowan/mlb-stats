6ae5136fc986264b69673a6a65290c976ee6b420
Louis Rowan - Vendor <louis.rowan@icloud.com>
2018-03-11 21:21:41 -0700

    basic functionality of parsing data and sending to FE

---
+++ src/server/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const Express = require('express');
+const Path = require('path');
+const Fs = require('fs');
+const Api = require('./api');
+
+const PORT = process.env.PORT || 3001;
+const app = Express();
+
+app.use(Express.static(__dirname + '/../../dist'));
+
+
+app.use('/api', Api);
+
+
+app.get('/', (req, res) => {
+  res.sendFile(Path.resolve(__dirname, '../../dist/index.html'));
+});
+
+
+app.listen(PORT, (err) => {
+
+    if (err) {
+        console.log("err starting server", err)
+    }
+    console.log('server started on port', PORT)
+});
